cb6cc9515422dc7a65435f8ccaf0ccc5d21a0d35
Gjorgjioska <marina.gorgioska@gmail.com>
2022-02-21 19:52:43 +0100

    Working on Dropdown Architecture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import { selectOptions } from "@testing-library/user-event/dist/select-options";
 
 const items = [
   {
@@ -17,10 +19,31 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A shade of Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState("options[0]");
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
